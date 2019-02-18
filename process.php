0b792b944ec21be151c9e121f2cd34789b70c3e7
Shairasepe <36975453+Shairasepe@users.noreply.github.com>
2019-02-17 19:25:28 -0800

    second commit

--- process.php
+++ process.php
@@ -4,19 +4,26 @@ session_start();
 $host = '';
 $db ='';
 
-$mysqli = new mysqli('localhost','root','','registration') or die(mysqli_error($mysqli));
+$mysqli = new mysqli('localhost','root','','clearance') or die(mysqli_error($mysqli));
 
 $id='';
 $update = false;
 $firstname ="";
 $lastname="";
-$contact="";
-
+$status="";
+$address="";
+$birthdate="";
+$barangay_id="";
+$staff_id="";
 
 if(isset($_POST['save'])){
 	$firstname = $_POST['firstname'];
 	$lastname = $_POST['lastname'];
-	$contact = $_POST['contact'];
+	$status = $_POST['status'];
+	$address = $_POST['address'];
+	$birthdate = $_POST['birthdate'];
+	$barangay_id = $_POST['barangay_id'];
+	$staff_id = $_POST['staff_id'];
 	$username= $_SESSION["username"];
 	$result=$mysqli->query("select id from users where username='$username'") or die($mysqli->error);
 	if(@count($result)==1)
@@ -26,7 +33,7 @@ if(isset($_POST['save'])){
 
 	}
 
-	$mysqli->query("INSERT INTO data (userid,firstname,lastname,contact) VALUES ('$userid','$firstname','$lastname','$contact')") or
+	$mysqli->query("INSERT INTO staff (userid,firstname,lastname,status,address,birthdate,barangay_id,staff_id) VALUES ('$userid','$firstname','$lastname','$status','$address','$birthdate','$barangay_id','$staff_id')") or
 			die($mysqli->error);
 	$_SESSION['message'] = "Record has been saved!";
 	$_SESSION['msg_type'] = "success";
@@ -36,7 +43,7 @@ if(isset($_POST['save'])){
 }
 if(isset($_GET['delete'])){
 	$id =$_GET['delete'];
-	$mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error());
+	$mysqli->query("DELETE FROM staff WHERE id=$id") or die($mysqli->error());
 	
 	$_SESSION['message'] = "Record has been deleted!";
 	$_SESSION['msg_type'] = "danger";
@@ -48,12 +55,16 @@ if(isset($_GET['delete'])){
 if(isset($_GET['edit'])){
 	$id = $_GET['edit'];
 	$update =true;
-	$result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error);
+	$result = $mysqli->query("SELECT * FROM staff WHERE id=$id") or die($mysqli->error);
 	if(@count($result)==1){
 		$row=$result->fetch_array();
 		$firstname = $row['firstname'];
 		$lastname = $row['lastname'];
-		$contact = $row['contact'];
+		$status = $row['status'];
+		$address = $row['address'];
+		$birthdate = $row['birthdate'];
+		$barangay_id = $row['barangay_id'];
+		$staff_id = $row['staff_id'];
 		
 	}
 }
@@ -61,9 +72,13 @@ if(isset($_POST['update'])){
 	$id = $_POST['id'];
 	$firstname = $_POST['firstname'];
 	$lastname = $_POST['lastname'];
-	$contact = $_POST['contact'];
+	$status = $_POST['status'];
+	$address = $_POST['address'];
+	$birthdate = $_POST['birthdate'];
+	$barangay_id = $_POST['barangay_id'];
+	$staff_id = $_POST['staff_id'];
 	
-	$mysqli->query("UPDATE data SET firstname='$firstname',lastname='$lastname',contact='$contact' WHERE id=$id") or die($mysqli->error);
+	$mysqli->query("UPDATE staff SET firstname='$firstname',lastname='$lastname',status='$status',address='$address',birthdate='$birthdate',barangay_id='$barangay_id',staff_id='$sta' WHERE id=$id") or die($mysqli->error);
 	$_SESSION['message'] = "Record has been updated!";
 	$_SESSION['msg_type'] = "warning";
 	
