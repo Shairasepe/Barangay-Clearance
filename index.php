2ea1c23f5f46a90b1b9c883d9f7e0309c868392d
Shairasepe <36975453+Shairasepe@users.noreply.github.com>
2019-03-03 23:56:29 -0800

    second commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<center>
+	<title>BarangayClearance</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<style>
+		body{
+			  background-image: url(ss.jpg);
+		}
+		</style>
+<body>
+	<h1>BARANGAY CLEARANCE</h1>
+    <h2>OROQUIETA CITY</h2>
+						<br><a href ="Astaff.php"><button type="button" class="btn btn-primary btn-lg">Add Staff</button>
+						<a href ="Aperson.php"><button type="button" class="btn btn-primary btn-lg">Add Person</button>
+						<a href ="Abarangay.php"><button type="button" class="btn btn-primary btn-lg">Add Barangay</button>
+						<a href ="Acaptain.php"><button type="button" class="btn btn-primary btn-lg">Add Captain</button>
+						<a href ="Acedula.php"><button type="button" class="btn btn-primary btn-lg">Add Cedula</button>
+						<a href ="Aclearance.php"><button type="button" class="btn btn-primary btn-lg">Add Clearance</button>
+						<br><a href ="login.php"><input class="btn" type="button" id="list_btn" value="LOGOUT"/></a></br>
+</center>
+ </body>
+ </html>
+
