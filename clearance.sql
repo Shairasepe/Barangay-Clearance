70ab987ae34f1384ede61a540998b65f0eda4302
Shairasepe <36975453+Shairasepe@users.noreply.github.com>
2019-03-25 20:38:45 -0700

    update clearance.sql

--- clearance.sql
+++ clearance.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2019 at 08:44 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Mar 25, 2019 at 05:36 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -27,8 +29,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `barangay` (
-  `id` int(100) NOT NULL,
-  `barangay_name` varchar(100) NOT NULL
+  `barangay_id` int(50) NOT NULL,
+  `barangay_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -38,10 +40,11 @@ CREATE TABLE `barangay` (
 --
 
 CREATE TABLE `captain` (
-  `id` int(100) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `middle_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL
+  `captain_id` int(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `barangay_id` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -51,36 +54,45 @@ CREATE TABLE `captain` (
 --
 
 CREATE TABLE `cedula` (
-  `id` int(100) NOT NULL,
+  `cedula_id` int(50) NOT NULL,
   `date` date NOT NULL,
-  `amount` int(100) NOT NULL,
-  `place` varchar(100) NOT NULL
+  `amount` int(50) NOT NULL,
+  `place` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `persons`
+-- Table structure for table `clearance`
 --
 
-CREATE TABLE `persons` (
-  `id` int(100) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `middle_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `status` varchar(100) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `birth_date` date NOT NULL
+CREATE TABLE `clearance` (
+  `clearance_id` int(50) NOT NULL,
+  `persons_id` int(50) DEFAULT NULL,
+  `barangay_id` int(50) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `amount` int(50) DEFAULT NULL,
+  `cedula_id` int(50) DEFAULT NULL,
+  `captain_id` int(50) DEFAULT NULL,
+  `staff_id` int(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `persons`
+-- Table structure for table `person`
 --
 
-INSERT INTO `persons` (`id`, `first_name`, `middle_name`, `last_name`, `status`, `address`, `birth_date`) VALUES
-(2342342, 'werwer', 'wewrw', 'last_name', 'sdfdfsdfsdfsdfsdfdf', 'sdfsdfd', '2019-02-22'),
-(1231234534, 'wqeqw12312', 'qweqwe2343', 'last_name', 'eqweqww', '1234233213qweqwewqe', '2019-01-31'),
-(2147483647, 'sxg', 'sdgs', 'last_name', 'bdo', 'derf', '2019-02-23');
+CREATE TABLE `person` (
+  `persons_id` int(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `birth_date` date NOT NULL,
+  `barangay_id` int(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -89,18 +101,12 @@ INSERT INTO `persons` (`id`, `first_name`, `middle_name`, `last_name`, `status`,
 --
 
 CREATE TABLE `staff` (
-  `id` int(100) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `middle_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL
+  `staff_id` int(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `barangay_id` int(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `staff`
---
-
-INSERT INTO `staff` (`id`, `first_name`, `middle_name`, `last_name`) VALUES
-(32, 'xdfd', 'sd', 'sdr');
 
 -- --------------------------------------------------------
 
@@ -109,18 +115,10 @@ INSERT INTO `staff` (`id`, `first_name`, `middle_name`, `last_name`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `id` int(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'se', 'se'),
-(2, 'qwe', 'qwee');
 
 --
 -- Indexes for dumped tables
@@ -130,31 +128,45 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 -- Indexes for table `barangay`
 --
 ALTER TABLE `barangay`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`barangay_id`);
 
 --
 -- Indexes for table `captain`
 --
 ALTER TABLE `captain`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`captain_id`),
+  ADD KEY `barangay_id` (`barangay_id`);
 
 --
 -- Indexes for table `cedula`
 --
 ALTER TABLE `cedula`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`cedula_id`);
 
 --
--- Indexes for table `persons`
+-- Indexes for table `clearance`
 --
-ALTER TABLE `persons`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `clearance`
+  ADD PRIMARY KEY (`clearance_id`),
+  ADD KEY `persons_id` (`persons_id`),
+  ADD KEY `barangay_id` (`barangay_id`),
+  ADD KEY `cedula_id` (`cedula_id`),
+  ADD KEY `captain_id` (`captain_id`),
+  ADD KEY `staff_id` (`staff_id`);
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`persons_id`),
+  ADD KEY `barangay_id` (`barangay_id`);
 
 --
 -- Indexes for table `staff`
 --
 ALTER TABLE `staff`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`staff_id`),
+  ADD KEY `barangay_id` (`barangay_id`);
 
 --
 -- Indexes for table `users`
@@ -170,7 +182,41 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `captain`
+--
+ALTER TABLE `captain`
+  ADD CONSTRAINT `captain_ibfk_1` FOREIGN KEY (`barangay_id`) REFERENCES `barangay` (`barangay_id`);
+
+--
+-- Constraints for table `clearance`
+--
+ALTER TABLE `clearance`
+  ADD CONSTRAINT `clearance_ibfk_1` FOREIGN KEY (`persons_id`) REFERENCES `person` (`persons_id`),
+  ADD CONSTRAINT `clearance_ibfk_2` FOREIGN KEY (`barangay_id`) REFERENCES `barangay` (`barangay_id`),
+  ADD CONSTRAINT `clearance_ibfk_3` FOREIGN KEY (`cedula_id`) REFERENCES `cedula` (`cedula_id`),
+  ADD CONSTRAINT `clearance_ibfk_4` FOREIGN KEY (`captain_id`) REFERENCES `captain` (`captain_id`),
+  ADD CONSTRAINT `clearance_ibfk_5` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`);
+
+--
+-- Constraints for table `person`
+--
+ALTER TABLE `person`
+  ADD CONSTRAINT `person_ibfk_1` FOREIGN KEY (`barangay_id`) REFERENCES `barangay` (`barangay_id`);
+
+--
+-- Constraints for table `staff`
+--
+ALTER TABLE `staff`
+  ADD CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`barangay_id`) REFERENCES `barangay` (`barangay_id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
