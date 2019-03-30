3106fd901e327aca1e3b0035a4af63ef934198d9
Shairasepe <36975453+Shairasepe@users.noreply.github.com>
2019-03-30 14:02:53 +0800

    first commit

--- clearance.sql
+++ clearance.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2019 at 05:36 AM
+-- Generation Time: Mar 30, 2019 at 02:30 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -33,6 +33,14 @@ CREATE TABLE `barangay` (
   `barangay_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `barangay`
+--
+
+INSERT INTO `barangay` (`barangay_id`, `barangay_name`) VALUES
+(2345, 'Lower Loboc'),
+(4564, 'Canubay');
+
 -- --------------------------------------------------------
 
 --
@@ -41,11 +49,18 @@ CREATE TABLE `barangay` (
 
 CREATE TABLE `captain` (
   `captain_id` int(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `middle_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `middlename` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
   `barangay_id` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `captain`
+--
+
+INSERT INTO `captain` (`captain_id`, `firstname`, `middlename`, `lastname`, `barangay_id`) VALUES
+(5678, 'Mario', 'Agora', 'Caban', 4564);
 
 -- --------------------------------------------------------
 
@@ -59,6 +74,14 @@ CREATE TABLE `cedula` (
   `amount` int(50) NOT NULL,
   `place` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cedula`
+--
+
+INSERT INTO `cedula` (`cedula_id`, `date`, `amount`, `place`) VALUES
+(6785, '2019-03-07', 20, 'oroquieta city'),
+(9999, '2019-03-06', 23, 'oroquieta city');
 
 -- --------------------------------------------------------
 
@@ -74,8 +97,16 @@ CREATE TABLE `clearance` (
   `amount` int(50) DEFAULT NULL,
   `cedula_id` int(50) DEFAULT NULL,
   `captain_id` int(50) DEFAULT NULL,
-  `staff_id` int(50) DEFAULT NULL
+  `staff_id` int(50) DEFAULT NULL,
+  `purpose` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `clearance`
+--
+
+INSERT INTO `clearance` (`clearance_id`, `persons_id`, `barangay_id`, `date`, `amount`, `cedula_id`, `captain_id`, `staff_id`, `purpose`) VALUES
+(4353, 6677, 2345, '2019-03-06', 23, 6785, 5678, 6643, 'scholarship');
 
 -- --------------------------------------------------------
 
@@ -94,6 +125,14 @@ CREATE TABLE `person` (
   `barangay_id` int(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `person`
+--
+
+INSERT INTO `person` (`persons_id`, `first_name`, `middle_name`, `last_name`, `status`, `address`, `birth_date`, `barangay_id`) VALUES
+(6677, 'Shaira', 'Bigcas', 'Sepe', 'single', 'oroquieta city', '2019-03-06', 2345),
+(45543, 'JD', 'Maisling', 'Carpo', 'married', 'oroquieta city', '2019-03-07', 4564);
+
 -- --------------------------------------------------------
 
 --
@@ -102,11 +141,19 @@ CREATE TABLE `person` (
 
 CREATE TABLE `staff` (
   `staff_id` int(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `middle_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
+  `Firstname` varchar(50) NOT NULL,
+  `Middlename` varchar(50) NOT NULL,
+  `Lastname` varchar(50) NOT NULL,
   `barangay_id` int(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`staff_id`, `Firstname`, `Middlename`, `Lastname`, `barangay_id`) VALUES
+(6643, 'Charlie', 'Valles', 'Cruz', 2345),
+(7871, 'Ann', 'Pasco', 'Castro', 4564);
 
 -- --------------------------------------------------------
 
@@ -115,10 +162,16 @@ CREATE TABLE `staff` (
 --
 
 CREATE TABLE `users` (
-  `id` int(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`) VALUES
+('admin', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -167,22 +220,6 @@ ALTER TABLE `person`
 ALTER TABLE `staff`
   ADD PRIMARY KEY (`staff_id`),
   ADD KEY `barangay_id` (`barangay_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
